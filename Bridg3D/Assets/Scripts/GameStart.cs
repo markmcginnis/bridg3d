09c59d1967bc3b544d684e80243d95d600cabaca
Mark McGinnis <mark.mcginnis@ufl.edu>
2022-06-05 13:48:38 -0400

    project setup continued, adding hierarchy and some sample scripts for reference and use

---
+++ Bridg3D/Assets/Scripts/GameStart.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerSpawn;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, playerSpawn.position, playerSpawn.rotation);
+    }
+
+    public void EndGame(bool win)
+    {
+        if (win)
+        {
+            UnityEngine.Debug.Log("won");
+            StartCoroutine(loadMenu("EndGameWin"));
+        }
+        else
+        {
+            UnityEngine.Debug.Log("lost");
+            StartCoroutine(loadMenu("EndGameLoss"));
+        }
+    }
+
+    IEnumerator loadMenu(string menu)
+    {
+        yield return new WaitForSeconds(3);
+
+        SceneManager.LoadScene(menu);
+
+        yield break;
+    }
+}
---
+++ Bridg3D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void Quit()
+    {
+        UnityEngine.Debug.Log("quit");
+        Application.Quit();
+    }
+}
