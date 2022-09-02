e8c81f0bd681f317b9b68ca4476b3d9498674012
Mark McGinnis <mark.mcginnis@ufl.edu>
2022-09-02 15:24:05 -0400

    made DefendController require HealthController b/c everything that can hold a shield should be able to have health

--- Bridg3D/Assets/Scripts/DefendController.cs
+++ Bridg3D/Assets/Scripts/DefendController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(HealthController))]
 public class DefendController : MonoBehaviour
 {
     public Animator shieldAnimator;
